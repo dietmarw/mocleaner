742e8260993dd3da8d0d21ddae1fbb1e4b147109
Dietmar Winkler <dietmar.winkler@dwe.no>
2014-07-18 23:48:28 +0200

    Include the walker into mocleaner.
    We keep walker.go for now.

--- mocleaner.go
+++ mocleaner.go
@@ -1,8 +1,17 @@
 package main
 
-import "os"
-import "strings"
-import "bufio"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 
 /* This function takes a string and returns
    a (potentially nil) error object */
@@ -40,4 +49,39 @@ func TTWS(filename string) error {
 	return nil;
 }
 
-func main() { TTWS("test.mo"); }
+func WalkFunc(path string, info os.FileInfo, err error) error {
+	/* list of directories to ignore */
+	blacklist := []string{".bzr", ".cvs", ".git", ".hg", ".svn"}
+	if contains(path, blacklist){
+		fmt.Printf("Skipping version control dir: %s\n", path)
+		return filepath.SkipDir
+	} else {
+		inf, err := os.Open(path)
+		defer inf.Close();
+		if (err!=nil) { inf.Close(); return err; }
+		readStart := io.LimitReader(inf, 512);
+		data, err := ioutil.ReadAll(readStart);
+		fileType := http.DetectContentType(data);
+		if strings.Contains(fileType, "text/plain"){
+			fmt.Printf("Trimming: %v\n", path)
+		} else {
+			fmt.Printf("Skipping file of type '%v': %v\n", fileType, path)
+		}
+	}
+	return nil
+}
+
+func contains(x string, a []string) bool {
+	for _, e := range(a) {
+		if (x==e) { return true; }
+	}
+	return false;
+}
+
+
+func main() {
+	flag.Parse()
+	root := flag.Arg(0)
+	err := filepath.Walk(root, WalkFunc)
+	fmt.Printf("filepath.Walk() returned %v\n", err)
+}
